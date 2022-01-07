eb84f64509c0aab7c41921d0d5f3387c7c5bdfd9
Irene <irenemanningm@gmail.com>
2022-01-06 20:36:36 -0600

    commit tree 46792ecaf873868fe7c1744150c93c60d2776b89

--- index.js
+++ index.js
@@ -1,4 +1,71 @@
-/* Your Code Here */
+function createEmployeeRecord([firstName, familyName, title, payPerHour]) {
+    return {
+        firstName: firstName,
+        familyName: familyName,
+        title: title,
+        payPerHour: payPerHour,
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+
+function createEmployeeRecords(employeeRecord) {
+    return employeeRecord.map((employee) => {
+        return createEmployeeRecord(employee)
+    }) 
+}
+
+function createTimeInEvent(timeStamp) {
+    
+    let [date, hour] = timeStamp.split(' ')
+        this.timeInEvents.push({
+            type: "TimeIn",
+            hour: parseInt(hour, 10),
+            date: date
+        })
+        return this
+}
+
+function createTimeOutEvent(timeStamp) {
+    
+    let [date, hour] = timeStamp.split(' ')
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(hour, 10),
+        date: date
+    })
+    return this
+}
+
+let hoursWorkedOnDate = function(date) {
+    
+    const inEvent = this.timeInEvents.find(event => event.date === date).hour
+    
+    const outEvent = this.timeOutEvents.find(event => event.date === date).hour
+    
+    return (outEvent - inEvent) / 100
+
+}
+
+let wagesEarnedOnDate = function(date) {
+    return this.payPerHour * hoursWorkedOnDate.call(this, date)
+}
+
+function calculatePayroll(employeeRecords) {
+    let sum = employeeRecords.reduce(function (acc, employeeRecord) {
+       return acc + allWagesFor.call(employeeRecord)
+    }, 0)
+    return sum
+}
+
+function findEmployeeByFirstName(employeeRecords, firstName) {
+    return employeeRecords.find(obj => {
+        if (obj.firstName === firstName) {
+            return obj
+        }  
+    });
+}
+
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
@@ -20,4 +87,5 @@ const allWagesFor = function () {
 
     return payable
 }
+
 
